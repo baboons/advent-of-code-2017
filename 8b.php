0e83331c5558e2fb84b37ad153716d3a24b08096
johan@baboons.se <uft-tH5-tFG-3s7>
2017-12-08 07:33:18 +0100

    Adds day 8

---
+++ 8b.php
@@ -0,0 +1,61 @@
+<?php
+
+$input = include 'utils/input.php';
+
+$register = [];
+$max = 0;
+
+foreach (explode("\n", $input) as $row) {
+
+    [$var, $op, $value, $if, $ifVar, $ifOp, $ifValue] = preg_split("/\s+/", $row);
+
+    if(!isset($register[$var])) $register[$var] = 0;
+    if(!isset($register[$ifVar])) $register[$ifVar] = 0;
+
+    $continue = false;
+
+    switch($ifOp) {
+        case '>':
+            if ($register[$ifVar] > $ifValue) $continue = true;
+            break;
+        case '<':
+            if ($register[$ifVar] < $ifValue) $continue = true;
+            break;
+        case '>=':
+            if ($register[$ifVar] >= $ifValue) $continue = true;
+            break;
+        case '<=':
+            if ($register[$ifVar] <= $ifValue) $continue = true;
+            break;
+        case '==':
+            if ($register[$ifVar] == $ifValue) $continue = true;
+            break;
+        case '!=':
+            if ($register[$ifVar] != $ifValue) $continue = true;
+            break;
+        default:
+            die("Invalid operand $ifOp" . PHP_EOL);
+            break;
+    }
+
+    if (!$continue) {
+        continue;
+    }
+
+    switch ($op) {
+        case 'inc':
+            $register[$var] += $value;
+            break;
+        case 'dec':
+            $register[$var] -= $value;
+            break;
+    }
+
+    if($register[$var] > $max) {
+        $max = $register[$var];
+    }
+
+}
+
+echo $max . PHP_EOL;
+
