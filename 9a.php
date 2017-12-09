f183a8b9f4d9fb8e67cc6f39bc1fdea177ab0dd8
johan@baboons.se <uft-tH5-tFG-3s7>
2017-12-09 07:19:46 +0100

    Adds day 9

---
+++ 9a.php
@@ -0,0 +1,33 @@
+<?php
+
+$input = include 'utils/input.php';
+
+$garbage = false;
+$ignore = 1;
+$level = 0;
+$score = 0;
+
+foreach (str_split($input) as $char) {
+    $ignore += 1;
+
+    if ($ignore >= 2 && $garbage && $char === '>') {
+        $garbage = false;
+    } elseif ($ignore >= 2 && $char === '!') {
+        $ignore = 0;
+    } elseif ($ignore >= 2 && !$garbage) {
+        switch($char) {
+            case '{':
+                $level += 1;
+                break;
+            case '}':
+                $score += $level;
+                $level -= 1;
+                break;
+            case '<':
+                $garbage = true;
+                break;
+        }
+    }
+}
+
+echo $score . PHP_EOL;
