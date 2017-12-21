8e66a9a4d6435817077216fae4e933981736cd00
johan@baboons.se <uft-tH5-tFG-3s7>
2017-12-21 11:33:50 +0100

    Day 21 - Fractal Art

---
+++ 21a.php
@@ -0,0 +1,24 @@
+<?php
+include 'utilities.php';
+
+$lines = explode(PHP_EOL, input());
+$rules = [];
+
+foreach ($lines as $line) {
+
+    $rule = explode(' => ', $line);
+    $matchRule = rule2array($rule[0]);
+    $matchRuleFlipped = flip($matchRule);
+    $rules[array2rule($matchRule)] = $rule[1];
+    $rules[array2rule($matchRuleFlipped)] = $rule[1];
+
+    foreach (range(1, 3) as $i) {
+        $matchRule = rotate($matchRule);
+        $matchRuleFlipped = rotate($matchRuleFlipped);
+        $rules[array2rule($matchRule)] = $rule[1];
+        $rules[array2rule($matchRuleFlipped)] = $rule[1];
+    }
+}
+$pattern = rule2array('.#./..#/###');
+
+echo pixelsByIterations($pattern, $rules, 5) . PHP_EOL;
--- utilities.php
+++ utilities.php
@@ -75,3 +75,135 @@ function knotHash(string $input, bool $binary = false): string {
 
     return $hash;
 }
+
+/**
+ * @param array $rule
+ * @return array
+ */
+function rotate(array $rule): array
+{
+    $return = [];
+
+    for ($i = 0; $i < count($rule[0]); $i++) {
+        for ($j = 0; $j < count($rule[0]); $j++) {
+            $value = $rule[count($rule[0]) - $j - 1][$i];
+
+            if (isset($return[$i])) {
+                $return[$i][] = $value;
+            } else {
+                $return[$i] = [$value];
+            }
+        }
+    }
+
+    return $return;
+}
+
+/**
+ * @param array $rule
+ * @return array
+ */
+function flip(array $rule): array
+{
+    return array_reverse($rule);
+}
+
+/**
+ * @param array $pattern
+ * @param string $count
+ * @return int
+ */
+function pixels(array $pattern, string $count = '#'): int
+{
+    $sum = 0;
+
+    foreach($pattern as $row) {
+        foreach($row as $value) {
+            if($value === $count) {
+                $sum++;
+            }
+        }
+    }
+
+    return $sum;
+}
+
+/**
+ * @param array $pattern
+ * @param array $rules
+ * @param int $iterations
+ * @return int
+ */
+function pixelsByIterations(array $pattern, array $rules, int $iterations): int
+{
+    foreach (range(1, $iterations) as $loop) {
+        $matchSize = count($pattern) % 2 == 0 ? 2 : 3;
+
+        $pos = [
+            'x' => 0,
+            'y' => 0,
+        ];
+
+        $rowOffset = 0;
+
+        $newPattern = [];
+
+        while (isset($pattern[$pos['y']][$pos['x']])) {
+            $block = [];
+
+            foreach (range($pos['y'], $pos['y'] + $matchSize - 1) as $i) {
+                $row = [];
+
+                foreach (range($pos['x'], $pos['x'] + $matchSize - 1) as $j) {
+                    $row[] = $pattern[$i][$j];
+                }
+
+                $block[] = $row;
+            }
+
+            $block = rule2array($rules[array2rule($block)]);
+
+            foreach ($block as $i => $row) {
+                if (isset($newPattern[$i + $rowOffset])) {
+                    $newPattern[$i + $rowOffset] = array_merge($newPattern[$i + $rowOffset], $row);
+                } else {
+                    $newPattern[$i + $rowOffset] = $row;
+                }
+            }
+
+            $pos['x'] += $matchSize;
+
+            if (!isset($pattern[$pos['y']][$pos['x']])) {
+                $pos['x'] = 0;
+                $pos['y'] += $matchSize;
+                $rowOffset += $matchSize + 1;
+            }
+        }
+
+        $pattern = $newPattern;
+    }
+
+    return pixels($pattern);
+}
+
+/**
+ * @param string $rule
+ * @return array
+ */
+function rule2array(string $rule): array
+{
+    return array_map(function($part) {
+        return str_split($part);
+    }, explode('/', $rule));
+}
+
+/**
+ * @param array $array
+ * @return string
+ */
+function array2rule(array $array): string
+{
+    return implode('/', array_map(function($parts) {
+        return implode('', $parts);
+    }, $array));
+}
